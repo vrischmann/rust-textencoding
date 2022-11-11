38135c7b7429e8c9b09911494070d688d2949a83
Vincent Rischmann <vincent@rischmann.fr>
2022-11-11 13:54:22 +0100

    fix DecodeError and add some tests

--- src/hex.rs
+++ src/hex.rs
@@ -32,10 +32,10 @@ pub fn encode(data: &[u8]) -> String {
     output
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub enum DecodeError {
     InvalidLength(usize),
-    InvalidChar(u8),
+    InvalidChar(char),
 }
 
 pub fn decode(data: &str) -> Result<Vec<u8>, DecodeError> {
@@ -54,11 +54,11 @@ pub fn decode(data: &str) -> Result<Vec<u8>, DecodeError> {
 
         let left_byte = REVERSE_HEX_ALPHABET[left as usize];
         if left_byte == 0xff {
-            return Err(DecodeError::InvalidChar(left));
+            return Err(DecodeError::InvalidChar(left as char));
         }
         let right_byte = REVERSE_HEX_ALPHABET[right as usize];
         if right_byte == 0xff {
-            return Err(DecodeError::InvalidChar(right));
+            return Err(DecodeError::InvalidChar(right as char));
         }
 
         let b = left_byte << 4 | right_byte;
@@ -76,7 +76,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn hex_encode_should_work() {
+    fn encode_should_work() {
         let input: Vec<u8> = vec![0xab, 0xad, 0xba, 0xac];
         let expected = "abadbaac";
 
@@ -85,11 +85,33 @@ mod tests {
     }
 
     #[test]
-    fn hex_decode_should_work() {
+    fn decode_should_work() {
         let input = "abadbaac";
         let expected: Vec<u8> = vec![0xab, 0xad, 0xba, 0xac];
 
         let result = decode(input).unwrap();
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn decode_of_non_hex_chars_should_fail() {
+        {
+            let input = "uv";
+            let result = decode(input);
+            assert_eq!(result.unwrap_err(), DecodeError::InvalidChar('u'));
+        }
+
+        {
+            let input = "av";
+            let result = decode(input);
+            assert_eq!(result.unwrap_err(), DecodeError::InvalidChar('v'));
+        }
+    }
+
+    #[test]
+    fn decode_of_invalid_length_string_should_fail() {
+        let input = "vvv";
+        let result = decode(input);
+        assert_eq!(result.unwrap_err(), DecodeError::InvalidLength(3));
     }
 }
