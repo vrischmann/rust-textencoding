0706c13edd1ad9332026c9092eef757239ad971b
Vincent Rischmann <vincent@rischmann.fr>
2022-11-13 20:32:40 +0100

    benches: add a benchmark for encode_slice

--- lib/benches/hex.rs
+++ lib/benches/hex.rs
@@ -3,9 +3,17 @@ use textencoding::hex;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("encode", |b| {
+        let data: [u8; 4] = [0xab, 0xbc, 0xbd, 0xcc];
         b.iter(|| {
-            let data: [u8; 4] = [0xab, 0xbc, 0xbd, 0xcc];
             hex::encode_lower(&data);
+        })
+    });
+
+    c.bench_function("encode_slice", |b| {
+        let data: [u8; 4] = [0xab, 0xbc, 0xbd, 0xcc];
+        let mut output = vec![0_u8; data.len() * 2];
+        b.iter(|| {
+            hex::encode_lower_slice(&data, &mut output).unwrap();
         })
     });
 }
